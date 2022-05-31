773189b2647ca4189f9b5de7b5184f5a36558b73
ashuhg <ashenafihagos.ah@gmail.com>
2022-05-31 22:22:21 +0300

    the rest of mandatory tasks

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(i):
-    if ord(i) >= ord('a') and ord(i) <= ord('z'):
+    if ord(i) >= ord('a') and ord(i) <= ord('a'):
         return True
     else:
         return False
